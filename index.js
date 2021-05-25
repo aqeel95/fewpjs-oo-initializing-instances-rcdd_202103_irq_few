60a11b49f7aec3d14aeac2a1213815d908244eb5
Aqeel Aloraibi <aqeel.aloraibi@gmail.com>
2021-05-25 08:44:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ class Dinner {
   constructor (salad,soup,entree,dessert){
     this.salad=salad;
     this.soup=soup;
-    this._entree=entree;
-    this.dessert=dessert;
+    this.entree=entree;
+    this._dessert=dessert;
   }
   }
